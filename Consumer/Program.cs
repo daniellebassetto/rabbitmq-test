17f7190334b8ad9eeeef5c3f5877900df42ae3eb
danibassetto <danibbassetto@hotmail.com>
2023-11-03 12:12:17 -0300

    new

---
+++ Consumer/Program.cs
@@ -0,0 +1,31 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory() { HostName = "localhost" };  // definimos uma conexão com um nó RabbitMQ em localhost
+
+using (var connection = factory.CreateConnection())  // abrimos uma conexão com um nó RabbitMQ
+
+using (var channel = connection.CreateModel())  // declaramos a fila a partir da qual iremos consumir as mensagens
+{
+    channel.QueueDeclare(queue: "saudacao_1",
+                         durable: false,
+                         exclusive: false,
+                         autoDelete: false,
+                         arguments: null);
+
+    var consumer = new EventingBasicConsumer(channel);  // solicita a entrega das mensagens de forma assíncrona e fornece um retorno de chamada
+
+    consumer.Received += (model, ea) =>  // recebe a mensagem da fila, converte para string e imprime no console
+    {
+        var body = ea.Body.ToArray();
+        var message = Encoding.UTF8.GetString(body);
+        Console.WriteLine($"[X] Recebida: {message}");
+    };
+
+    channel.BasicConsume(queue: "saudacao_1",  // indicando o consumo da mensagem para que ela saia da fila
+                         autoAck: true,
+                         consumer: consumer);
+}
+
+// a mensagem estará visivel em localhost:15672 na aba Queues and Streams
--- Publisher/Program.cs
+++ Publisher/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory() { HostName = "localhost" };  // definimos uma conexão com um nó RabbitMQ em localhost
+
+using (var connection = factory.CreateConnection())  // abrimos uma conexão com um nó RabbitMQ
+
+using (var channel = connection.CreateModel())  // criamos um canal onde vamos definir uma fila, uma mensagem e publicar a mensagem
+{
+    channel.QueueDeclare(queue: "saudacao_1",  // nome da fila
+                         durable: false,  // se for true, a fila permanece ativa após o servidor ser reiniciado
+                         exclusive: false,  // se for true ela só pode ser acessada via conexão atual e são excluídas ao fechar a conexão
+                         autoDelete: false, // se for true ela será deletada automaticamente após os consumidores usarem a fila
+                         arguments: null);
+
+    string message = "Bem vindo ao RabbitMQ";  // criando a mensagem
+
+    var body = Encoding.UTF8.GetBytes(message);  // codificamos a mensagem para um array de bytes, pois uma mensagem RabbitMQ é um bloco de dado binário
+
+    channel.BasicPublish(exchange: "",  // publicamos a mensagem informando a fila e o corpo da mensagem
+                        routingKey: "saudacao_1",
+                        basicProperties: null,
+                        body: body);
+
+    Console.WriteLine($"[X] Enviada: {message}");  // exibindo no terminal
+}
+
+Console.ReadLine();
+
+// a mensagem estará visivel em localhost:15672 na aba Queues and Streams
